a7252801f224cae849d9ecd9475a63dba08dd318
jackchuka <jackchuka@users.noreply.github.com>
2026-01-24 18:19:04 +0000

    Commit from GitHub Actions (Scheduled Update of Formulae)

--- Formula/mdschema.rb
+++ Formula/mdschema.rb
@@ -1,27 +1,27 @@
 class Mdschema < Formula
   desc '📝 A declarative schema-based Markdown validator that helps maintain consistent documentation structure across projects.'
-  version '0.5.0'
+  version '0.5.1'
   homepage 'https://github.com/jackchuka/mdschema'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/jackchuka/mdschema/releases/download/v0.5.0/mdschema_0.5.0_darwin_arm64.tar.gz'
-      sha256 'c11cc536ce5454701f34f53a021d32a0452f92c512847c53f36d81f5d28c8ccc'
+      url 'https://github.com/jackchuka/mdschema/releases/download/v0.5.1/mdschema_0.5.1_darwin_arm64.tar.gz'
+      sha256 '253d086fcf42b62d4e1e756a62a7de4a9a72c031f29833153af27f3964cbfc9e'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/jackchuka/mdschema/releases/download/v0.5.0/mdschema_0.5.0_darwin_amd64.tar.gz'
-      sha256 '41536311dec02f6544b304f63315b2a7f2444bfdeee95e6861257d67340cd1b5'
+      url 'https://github.com/jackchuka/mdschema/releases/download/v0.5.1/mdschema_0.5.1_darwin_amd64.tar.gz'
+      sha256 '11b5f6e4ece2870ae3fbf325df2f4b6439e843071461d45843c8382906e90dc8'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/jackchuka/mdschema/releases/download/v0.5.0/mdschema_0.5.0_linux_arm64.tar.gz'
-      sha256 '40e5be3b9e60e4a2998cfc25f54fc30bdb9cfecd7e7002ad810b2d6ed598c317'
+      url 'https://github.com/jackchuka/mdschema/releases/download/v0.5.1/mdschema_0.5.1_linux_arm64.tar.gz'
+      sha256 '9f94bbb1fb3aa5614d7f6c57136c2f47fe144e3f062980828c6baf28e9bfc927'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/jackchuka/mdschema/releases/download/v0.5.0/mdschema_0.5.0_linux_amd64.tar.gz'
-      sha256 '6293078692fc1accab322ae62ef91eaf443408bc5c3c54aba57b6b94862d0e15'
+      url 'https://github.com/jackchuka/mdschema/releases/download/v0.5.1/mdschema_0.5.1_linux_amd64.tar.gz'
+      sha256 '447f9f9602690cdb8fc466251d8449341220232ea2dc946f3232f926ff21b024'
     end
   end
 
