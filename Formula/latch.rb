d6ce34fabe2b3f6b24b926c6c199cc18df5d7ecd
jackchuka <jack16.m.u@gmail.com>
2026-03-09 11:04:08 +0900

    bump: latch

--- Formula/latch.rb
+++ Formula/latch.rb
@@ -1,27 +1,27 @@
 class Latch < Formula
   desc 'Task runner with approval gates. Define multi-step command pipelines, gate irreversible actions behind human approval, and optionally schedule them with cron.'
-  version '0.3.1'
+  version '0.3.2'
   homepage 'https://github.com/jackchuka/latch'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/jackchuka/latch/releases/download/v0.3.1/latch_0.3.1_darwin_arm64.tar.gz'
-      sha256 '1dafac9a9bb7275025036299a96a0707ba928e9b11b81bbc83682d93700d3bb8'
+      url 'https://github.com/jackchuka/latch/releases/download/v0.3.2/latch_0.3.2_darwin_arm64.tar.gz'
+      sha256 'a3f7a7a6757383d839c8e58bfcb627c9082c8f10bedc1a837da39388fecdc67f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/jackchuka/latch/releases/download/v0.3.1/latch_0.3.1_darwin_amd64.tar.gz'
-      sha256 '2f3d8f33cd307be69333f080aba3e6cda9bdd1c4872a6861ebc1bde6d5599d9c'
+      url 'https://github.com/jackchuka/latch/releases/download/v0.3.2/latch_0.3.2_darwin_amd64.tar.gz'
+      sha256 '776f4558a61b884cd36f17fb3322cad1426c2b7a10073e69c14ce27fedef1dd2'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/jackchuka/latch/releases/download/v0.3.1/latch_0.3.1_linux_arm64.tar.gz'
-      sha256 '369d6039f1bc8bcf210b8500e04e3aadd93f11ea4782e789d1a23d98427017c4'
+      url 'https://github.com/jackchuka/latch/releases/download/v0.3.2/latch_0.3.2_linux_arm64.tar.gz'
+      sha256 'b600d8b8c290256ca52ebebb081680a8e92d46b90e25c0582db82e3c86b29c82'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/jackchuka/latch/releases/download/v0.3.1/latch_0.3.1_linux_amd64.tar.gz'
-      sha256 '2a87797aa37b674e7770b5693bd9aec25507de810ef99e2cb29b320d4806b527'
+      url 'https://github.com/jackchuka/latch/releases/download/v0.3.2/latch_0.3.2_linux_amd64.tar.gz'
+      sha256 '7bcb4d3885b554f1938960388624ab119314157cf4c1bb6a2c058e270c108816'
     end
   end
 
