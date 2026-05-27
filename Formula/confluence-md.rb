fa1b6e12f8277830434f8ea7f59a28716f21e588
jackchuka <jackchuka@users.noreply.github.com>
2026-05-27 05:07:47 +0000

    Commit from GitHub Actions (Scheduled Update of Formulae and Casks)

--- Formula/confluence-md.rb
+++ Formula/confluence-md.rb
@@ -1,27 +1,27 @@
 class ConfluenceMd < Formula
   desc '🧭 From Confluence to clean Markdown, images and all — just one command'
-  version '0.3.13'
+  version '0.3.14'
   homepage 'https://github.com/jackchuka/confluence-md'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/jackchuka/confluence-md/releases/download/v0.3.13/confluence-md_0.3.13_darwin_arm64.tar.gz'
-      sha256 '21adf0d4e085fd33b6bc3c8338dbd3a018f64218772913f590363faa1316b3d1'
+      url 'https://github.com/jackchuka/confluence-md/releases/download/v0.3.14/confluence-md_0.3.14_darwin_arm64.tar.gz'
+      sha256 '7cf47ef8ee576a5d26425e41e80800a0f3ae300a577ae15b9b0beaf28a196dfb'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/jackchuka/confluence-md/releases/download/v0.3.13/confluence-md_0.3.13_darwin_amd64.tar.gz'
-      sha256 'f991e684a387621b0e1e1a41352741278ee4bd0ccbcbebab42e5269afbabad3d'
+      url 'https://github.com/jackchuka/confluence-md/releases/download/v0.3.14/confluence-md_0.3.14_darwin_amd64.tar.gz'
+      sha256 'fcb6f18fd5964e57f02f90ba6d7ed66e15b5ec9535994b214abf0bf431a322b0'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/jackchuka/confluence-md/releases/download/v0.3.13/confluence-md_0.3.13_linux_arm64.tar.gz'
-      sha256 '9496fed07721ff9198fa60d331b511ed2ff1017553898db9a670340fbf0aba68'
+      url 'https://github.com/jackchuka/confluence-md/releases/download/v0.3.14/confluence-md_0.3.14_linux_arm64.tar.gz'
+      sha256 '05671d7c45383e1d6a0852f53e67875a35a817aadd84daa8fa5c4c8e4e740e49'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/jackchuka/confluence-md/releases/download/v0.3.13/confluence-md_0.3.13_linux_amd64.tar.gz'
-      sha256 '2972ed1a5f13bc3e97ebd5cf1245c002494498ecb18abb38ad10d76c3b5e0e1f'
+      url 'https://github.com/jackchuka/confluence-md/releases/download/v0.3.14/confluence-md_0.3.14_linux_amd64.tar.gz'
+      sha256 '90417722921c3939db566d5134fce68f98a228476cd2acae181d39545a564956'
     end
   end
 
