c2b081b7bcf05c3c5b35a29110690cc92d3b7682
jackchuka <jackchuka@users.noreply.github.com>
2026-06-04 02:31:20 +0000

    Commit from GitHub Actions (Scheduled Update of Formulae and Casks)

--- Casks/timestack.rb
+++ Casks/timestack.rb
@@ -1,13 +1,13 @@
 cask "timestack" do
-  version "0.3.5"
+  version "0.3.6"
 
   on_arm do
-    sha256 "b1fc81a9d3346ef5b9b1270e34baeebc520d7c02d7a1c3ef28bf74e9606ecf1d"
+    sha256 "7e6fb5e81b7864f62107ac777b4f7df0ed5239cd6c2c6e0f30e116757f0d36e7"
 
     url "https://github.com/jackchuka/timestack/releases/download/v#{version}/timestack_#{version}_aarch64.dmg"
   end
   on_intel do
-    sha256 "a997554cd5d3cac5308827b99d5b7e77e7c211378a6ac8c0619eb859cb0caa8c"
+    sha256 "d7fc55d4dc516c46ce4c6b62589121cb71640203f5491579064151a89f7e308a"
 
     url "https://github.com/jackchuka/timestack/releases/download/v#{version}/timestack_#{version}_x64.dmg"
   end
