d4b151d0ae8c858bde3c1bf634186144963320ed
jackchuka <jackchuka@users.noreply.github.com>
2026-09-07 03:20:11 +0000

    Commit from GitHub Actions (Scheduled Update of Formulae and Casks)

--- Formula/git-customs.rb
+++ Formula/git-customs.rb
@@ -1,27 +1,27 @@
 class GitCustoms < Formula
   desc 'Pre-push gate that catches PII and secrets before they reach a public remote'
-  version '0.1.5'
+  version '0.1.6'
   homepage 'https://github.com/jackchuka/git-customs'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/jackchuka/git-customs/releases/download/v0.1.5/git-customs_0.1.5_darwin_arm64.tar.gz'
-      sha256 '17a3559cdff07ab9c78ce2dd0be71b56f0ca3bb034168360de0a124275ebdf2a'
+      url 'https://github.com/jackchuka/git-customs/releases/download/v0.1.6/git-customs_0.1.6_darwin_arm64.tar.gz'
+      sha256 '1a09ff75eabb37492d7a18829816ab66163a429ded6d2830b17fba8631860fa8'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/jackchuka/git-customs/releases/download/v0.1.5/git-customs_0.1.5_darwin_amd64.tar.gz'
-      sha256 '962dd344c5d739dde6734e29fc14c815a3916d0d90eddcbcdefac77603578ff2'
+      url 'https://github.com/jackchuka/git-customs/releases/download/v0.1.6/git-customs_0.1.6_darwin_amd64.tar.gz'
+      sha256 '8fa293df453db0823a348b9358ab403e5e65d0cb2f09fa28c3607a01c3823b57'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/jackchuka/git-customs/releases/download/v0.1.5/git-customs_0.1.5_linux_arm64.tar.gz'
-      sha256 'd3a183914a8344184ab35f9671eaf28ddac7a9eed0c0a40a985380e1d92a185f'
+      url 'https://github.com/jackchuka/git-customs/releases/download/v0.1.6/git-customs_0.1.6_linux_arm64.tar.gz'
+      sha256 'c4d99cec1be3b7ed718502f7de9f3b0daf939fc0d50191cdda8a134f2d24ecb1'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/jackchuka/git-customs/releases/download/v0.1.5/git-customs_0.1.5_linux_amd64.tar.gz'
-      sha256 'cb8b11af69191650b26be45ad08a4fc5eb3cf8331d2334b941e108a3e99250cc'
+      url 'https://github.com/jackchuka/git-customs/releases/download/v0.1.6/git-customs_0.1.6_linux_amd64.tar.gz'
+      sha256 '57e42851307a39d53341bc092edeb4eb3182b3880a40a8989927721559a9f97d'
     end
   end
 
